daf84fdd6ee3583032740f5d335f6a3df551c143
Luke Herrington <lherrington100@gmail.com>
2016-08-28 22:18:11 -0700

    Add caching and some style.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const cache = require('memory-cache');
 const app = express();
 
 const PageImageColorComparator = require('./lib/PageImageColorComparator');
@@ -11,27 +12,55 @@ const opts = {
   colorNameDataAttr: 'option-title'
 };
 
+function template(text) {
+  return `
+  <html>
+    <head>
+      <link rel="stylesheet" type="text/css" href="http://cloud.typography.com/7738032/768302/css/fonts.css">
+      <style>
+        body {
+          text-align: center;
+          background-color: #35aa4e;
+          font-size: 3rem;
+          margin-top: 6rem;
+          color: white;
+          font-family: "Whitney a", "Whitney b", "Helvetica Neue", Helvetica, arial, sans-serif;;
+        }
+      </style>
+    </head>
+    <body>${text}</body>
+  </html>`;
+}
+
+function existsResponse(exists, res) {
+  if (exists) {
+      res.send(template('OMG OMG OMG. Finally!'));
+  } else {
+    res.send(template('Nope, not yet'));
+  }
+}
 
 app.get('/', function (req, res) {
   const comparator = new PageImageColorComparator(opts);
-  comparator.getPage()
-    .then(html => comparator.getColors(html))
-    .then(colors => Promise.all(colors.map(color => comparator.checkColors(color))))
-    .then(dEValues => dEValues.map(dE => comparator.evaluateColorCloseness(dE)))
-    .then(closenessVals => {
-      const closeColorExists = closenessVals.some(exists => exists);
-      if (closeColorExists) {
-        res.send('I can haz');        
-      } else {
-        res.send('Nope, not yet');        
-      }
-    })
-    .catch(e => {
-      res.sendStatus(500);
-    });
+  const exists = cache.get('exists');
+  if (exists === null) {
+    comparator.getPage()
+      .then(html => comparator.getColors(html))
+      .then(colors => Promise.all(colors.map(color => comparator.checkColors(color))))
+      .then(dEValues => dEValues.map(dE => comparator.evaluateColorCloseness(dE)))
+      .then(closenessVals => {
+        const closeColorExists = closenessVals.some(exists => exists);
+        cache.put('exists', closeColorExists, 240000);
+        existsResponse(closeColorExists, res);
+      })
+      .catch(e => {
+        res.status(500).send(template('Something broke!'));
+      });
+  } else {
+    existsResponse(exists, res);
+  }
 });
 
 app.listen(3000, function () {
   console.log('Listening on port 3000.');
 });
-
